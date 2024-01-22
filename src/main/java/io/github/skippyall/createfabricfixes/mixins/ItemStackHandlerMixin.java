ab45b8c860a39e3dbf1dd3fda05c35ebde428d25
skippyall <>
2024-01-22 19:47:19 +0100

    Actually 1.19.2 now

--- src/main/java/io/github/skippyall/createfabricfixes/mixins/ItemStackHandlerMixin.java
+++ src/main/java/io/github/skippyall/createfabricfixes/mixins/ItemStackHandlerMixin.java
@@ -1,27 +1,32 @@
 package io.github.skippyall.createfabricfixes.mixins;
 
-import org.spongepowered.asm.mixin.Mixin;
-
-import io.github.fabricators_of_create.porting_lib.core.util.INBTSerializable;
-import io.github.fabricators_of_create.porting_lib.transfer.item.ItemStackHandler;
-import io.github.fabricators_of_create.porting_lib.transfer.item.SlottedStackStorage;
-import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
-import net.minecraft.nbt.CompoundTag;
-
 import java.util.Iterator;
 
-@Mixin(ItemStackHandler.class)
-public abstract class ItemStackHandlerMixin implements SlottedStackStorage, INBTSerializable<CompoundTag> {
-	@SuppressWarnings("UnstableApiUsage")
-	@Override
-	public Iterable<StorageView<ItemVariant>> nonEmptyViews() {
-		return SlottedStackStorage.super.nonEmptyViews();
-	}
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
-	@SuppressWarnings("UnstableApiUsage")
+import com.google.common.collect.Iterators;
+
+import io.github.fabricators_of_create.porting_lib.extensions.INBTSerializable;
+import io.github.fabricators_of_create.porting_lib.transfer.ExtendedStorage;
+import io.github.fabricators_of_create.porting_lib.transfer.item.ItemStackHandler;
+import io.github.fabricators_of_create.porting_lib.transfer.item.ItemStackHandlerSnapshot;
+import io.github.fabricators_of_create.porting_lib.transfer.item.SlotExposedStorage;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.base.SnapshotParticipant;
+import net.minecraft.nbt.CompoundTag;
+
+@Mixin(ItemStackHandler.class)
+@SuppressWarnings("UnstableApiUsage")
+public abstract class ItemStackHandlerMixin extends SnapshotParticipant<ItemStackHandlerSnapshot> implements Storage<ItemVariant>, ExtendedStorage<ItemVariant>, INBTSerializable<CompoundTag>, SlotExposedStorage {
+	@Shadow
+	public abstract @NotNull Iterator<StorageView<ItemVariant>> iterator();
+
 	@Override
-	public Iterator<StorageView<ItemVariant>> nonEmptyIterator() {
-		return SlottedStackStorage.super.nonEmptyIterator();
+	public Iterator<StorageView<ItemVariant>> nonEmptyViews() {
+		return Iterators.filter(iterator(), view -> !view.isResourceBlank() && view.getAmount() > 0);
 	}
 }
